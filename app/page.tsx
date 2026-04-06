0ae727e5f63ac954a963e81d343f1f8b195fc121
shandohowell <shandohowell94@gmail.com>
2026-04-06 00:34:01 -0500

    Worked on UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import  Link from "next/link";
+
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center h-full">
-      <a href="/chat" className="text-black border-b-blue-400 bg-blue-400 p-4">Engage</a>
+      <Link href="/chat" className="text-black border-b-blue-400 bg-blue-400 p-4">Engage</Link>
     </div>
   );
 }
